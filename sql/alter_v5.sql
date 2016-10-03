5b2f9321c26489c67d5951e32534d4149872b92c
Geovanni <gescalante@xpectrumtech.com>
2016-10-03 09:02:31 -0500

    Se modifica nombre de tabla en script

--- sql/alter_v5.sql
+++ sql/alter_v5.sql
@@ -3,6 +3,6 @@
 -- Alter structure for table `apps`
 --
 
-ALTER TABLE `calendar`.`apps` ADD COLUMN `html_confirmation_email` TEXT NOT NULL after `from_name`;
-ALTER TABLE `calendar`.`apps` ADD COLUMN `html_modify_email` TEXT NOT NULL after `html_confirmation_email`;
-ALTER TABLE `calendar`.`apps` ADD COLUMN `html_cancel_email` TEXT NOT NULL after `html_modify_email`;
+ALTER TABLE `calendars`.`apps` ADD COLUMN `html_confirmation_email` TEXT NOT NULL after `from_name`;
+ALTER TABLE `calendars`.`apps` ADD COLUMN `html_modify_email` TEXT NOT NULL after `html_confirmation_email`;
+ALTER TABLE `calendars`.`apps` ADD COLUMN `html_cancel_email` TEXT NOT NULL after `html_modify_email`;
