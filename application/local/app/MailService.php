f8533b36e2bea2a1cddf04468bf53dc28d160481
Geovanni <gescalante@xpectrumtech.com>
2016-10-03 11:56:49 -0500

    Se agrego cache al consumir el servicio para obtener el token

--- application/local/app/MailService.php
+++ application/local/app/MailService.php
@@ -52,82 +52,73 @@ class MailService {
                                     $html_applyer = $_app->html_confirmation_email;
                                     $html_owner = $_app->html_confirmation_email;
                                     $subject = base64_encode(config('calendar.subject_confirmation_email'));
+                                    $html_text = 'confirmación';
                                     break;
                                 case 'modify':
                                     $html_applyer = $_app->html_modify_email;
                                     $html_owner = $_app->html_modify_email;
                                     $subject = base64_encode(config('calendar.subject_modify_email'));
+                                    $html_text = 'modificación';
                                     break;
                                 case 'cancel':
                                     $html_applyer = $_app->html_cancel_email;
                                     $html_owner = $_app->html_cancel_email;
                                     $subject = base64_encode(config('calendar.subject_cancel_email'));
+                                    $html_text = 'cancelación';
                                     break;
                                 default :               
                             }
-                        }                        
+                        } 
                         
-                        $appointment = new AppointmentRepository();
-                        $appointments = $appointment->listAppointmentById($appkey, $domain, $appointment_id);
-
-                        if (isset($appointments['error']) && $appointments['error'] !== null) {
+                        if (empty($html_applyer)) {
                             $res['error'] = true;
-                            $res['errorMessage'] = 'Hubo un error al obtener información de la cita. Descripción: ' . $appointments['error']->getMessage();
-                        } else {
-                            if (isset($appointments['data']) && (int)$appointments['count'] > 0) {
-                                foreach ($appointments['data'] as $a) { 
-                                    $date = new \DateTime($a->appointment_start_time);
-                                    $dia = $date->format('d');
-                                    $mes = $this->monthToSpanish($date->format('M'));
-                                    $ano = $date->format('Y');
-                                    $hora = $date->format('H:i');
-                                    $fecha_cita = $dia . ' de ' . $mes . ' de ' . $ano . ' a las ' . $hora;
-                                    $nombre_ciudadano = trim($a->applyer_name);
-                                    $nombre_funcionario = trim($a->owner_name);
-                                    $nombre_tramite = 'TRÁMITE';
-                                    $nombre_agenda = trim($a->name);
-                                    $motivo_cancelacion = !empty($a->cancelation_cause) ? trim($a->cancelation_cause) : 'No indicado';
-                                    $applyer_email = trim($a->applyer_email);
-                                    $owner_email =  trim($a->owner_email);
-                                    
-                                    if (!empty($a->metadata)) {
-                                        $metadata = json_decode($a->metadata, true);
-                                        if (isset($metadata['nombre_tramite']) && !empty($metadata['nombre_tramite'])) {
-                                            $nombre_tramite = trim($metadata['nombre_tramite']);
+                            $res['errorMessage'] = 'Debe configurar la plantilla de correo electrónico de ' . $html_text;
+                        } else {    
+                            $appointment = new AppointmentRepository();
+                            $appointments = $appointment->listAppointmentById($appkey, $domain, $appointment_id);
+
+                            if (isset($appointments['error']) && $appointments['error'] !== null) {
+                                $res['error'] = true;
+                                $res['errorMessage'] = 'Hubo un error al obtener información de la cita. Descripción: ' . $appointments['error']->getMessage();
+                            } else {
+                                if (isset($appointments['data']) && (int)$appointments['count'] > 0) {
+                                    foreach ($appointments['data'] as $a) { 
+                                        $date = new \DateTime($a->appointment_start_time);
+                                        $dia = $date->format('d');
+                                        $mes = $this->monthToSpanish($date->format('M'));
+                                        $ano = $date->format('Y');
+                                        $hora = $date->format('H:i');
+                                        $fecha_cita = $dia . ' de ' . $mes . ' de ' . $ano . ' a las ' . $hora;
+                                        $nombre_ciudadano = trim($a->applyer_name);
+                                        $nombre_funcionario = trim($a->owner_name);
+                                        $nombre_tramite = 'TRÁMITE';
+                                        $nombre_agenda = trim($a->name);
+                                        $motivo_cancelacion = !empty($a->cancelation_cause) ? trim($a->cancelation_cause) : 'No indicado';
+                                        $applyer_email = trim($a->applyer_email);
+                                        $owner_email =  trim($a->owner_email);
+
+                                        if (!empty($a->metadata)) {
+                                            $metadata = json_decode($a->metadata, true);
+                                            if (isset($metadata['nombre_tramite']) && !empty($metadata['nombre_tramite'])) {
+                                                $nombre_tramite = trim($metadata['nombre_tramite']);
+                                            }
                                         }
-                                    }
-                                    
-                                    $search = array(
-                                        '{{nombre_app}}' ,
-                                        '{{nombre}}',
-                                        '{{dia}}',
-                                        '{{mes}}',
-                                        '{{ano}}',
-                                        '{{hora}}',
-                                        '{{fecha_cita}}',                                        
-                                        '{{nombre_tramite}}',
-                                        '{{nombre_agenda}}',
-                                        '{{motivo_cancelacion}}',
-                                    );
-                                    $owner_replace = array(
-                                        $nombre_app,
-                                        $nombre_funcionario,
-                                        $dia,
-                                        $mes,
-                                        $ano,
-                                        $hora,
-                                        $fecha_cita,
-                                        $nombre_tramite,
-                                        $nombre_agenda,
-                                        $motivo_cancelacion,
-                                    );
-                                    $body = base64_encode(str_replace($search, $owner_replace, $html_owner));
-                                    
-                                    $send_mail_owner = $this->sendMail($from_email, $subject, $body, array($owner_email));
-                                    if (!$send_mail_owner['error']) {
-                                        $applyer_replace = array(
+
+                                        $search = array(
+                                            '{{nombre_app}}' ,
+                                            '{{nombre}}',
+                                            '{{dia}}',
+                                            '{{mes}}',
+                                            '{{ano}}',
+                                            '{{hora}}',
+                                            '{{fecha_cita}}',                                        
+                                            '{{nombre_tramite}}',
+                                            '{{nombre_agenda}}',
+                                            '{{motivo_cancelacion}}',
+                                        );
+                                        $owner_replace = array(
                                             $nombre_app,
-                                            $nombre_ciudadano,
+                                            $nombre_funcionario,
                                             $dia,
                                             $mes,
                                             $ano,
@@ -137,22 +128,39 @@ class MailService {
                                             $nombre_agenda,
                                             $motivo_cancelacion,
                                         );
-                                        $body = base64_encode(str_replace($search, $applyer_replace, $html_applyer));
-                                        $send_mail_applyer = $this->sendMail($from_email, $subject, $body, array($applyer_email));
-                                        if ($send_mail_applyer['error']) {
+                                        $body = base64_encode(str_replace($search, $owner_replace, $html_owner));
+
+                                        $send_mail_owner = $this->sendMail($from_email, $subject, $body, array($owner_email));
+                                        if (!$send_mail_owner['error']) {
+                                            $applyer_replace = array(
+                                                $nombre_app,
+                                                $nombre_ciudadano,
+                                                $dia,
+                                                $mes,
+                                                $ano,
+                                                $hora,
+                                                $fecha_cita,
+                                                $nombre_tramite,
+                                                $nombre_agenda,
+                                                $motivo_cancelacion,
+                                            );
+                                            $body = base64_encode(str_replace($search, $applyer_replace, $html_applyer));
+                                            $send_mail_applyer = $this->sendMail($from_email, $subject, $body, array($applyer_email));
+                                            if ($send_mail_applyer['error']) {
+                                                $res['error'] = true;
+                                                $res['errorMessage'] = $send_mail_applyer['errorMessage'];
+                                            }
+                                        } else {
                                             $res['error'] = true;
-                                            $res['errorMessage'] = $send_mail_applyer['errorMessage'];
+                                            $res['errorMessage'] = $send_mail_owner['errorMessage'];
                                         }
-                                    } else {
-                                        $res['error'] = true;
-                                        $res['errorMessage'] = $send_mail_owner['errorMessage'];
                                     }
+                                } else {
+                                    $res['error'] = true;
+                                    $res['errorMessage'] = 'El Id de la cita enviada no se encontró en la base de datos';
                                 }
-                            } else {
-                                $res['error'] = true;
-                                $res['errorMessage'] = 'El Id de la cita enviada no se encontró en la base de datos';
                             }
-                        }                       
+                        }
                     } else {
                         $res['error'] = true;
                         $res['errorMessage'] = 'La appkey y/o domain no se encontraron en la base de datos';
@@ -200,8 +208,9 @@ class MailService {
             $client = new Client();        
             $cache_id = 'cacheGetTokenSendMail';            
             $response_token = Cache::get($cache_id);
-            $response_token = null; // Ojo Borrar
-                    
+            $resp = array();
+            $access_token = '';
+            
             if ($response_token === null) {                
                 
                 // Obtiene el access token
@@ -214,16 +223,17 @@ class MailService {
                     ]
                 ]);
                 
-                if ($response_token->getStatusCode() == 200) {
-                    $resp = json_decode($response_token->getBody(), true);                    
+                $resp = json_decode($response_token->getBody(), true);
+                if ($response_token->getStatusCode() == 200) {                    
                     $ttl = (int)$resp['expires_in']/60;
-                    Cache::put($cache_id, $response_token, $ttl);
+                    Cache::put($cache_id, $resp, $ttl);
+                    $access_token = $resp['access_token'];
                 }
-            }
+            } else {                
+                $access_token = $response_token['access_token'];                
+            }            
             
-            if ($response_token->getStatusCode() == 200) {
-                $resp = json_decode($response_token->getBody(), true);                
-                $acces_token = $resp['access_token'];
+            if (!empty($access_token)) {
                 
                 // Envio el correo
                 $body = array(
@@ -236,19 +246,23 @@ class MailService {
                 );
                 
                 $response = Request::post($urlSendMail)
-                    ->addHeaders(array('Authorization' => 'Bearer ' . $acces_token))
+                    ->addHeaders(array('Authorization' => 'Bearer ' . $access_token))
                     ->expectsJson()
                     ->sendsJson()
                     ->body(json_encode($body))
                     ->send();
                 
-                if ($response->code != 200) {
+                if ($response->code == 401 && isset($response->body->error) &&
+                        $response->body->error == 'invalid_token') {                    
+                    Cache::forget($cache_id);
+                    $this->sendMail($from, $subject, $body, $to);
+                } else if ($response->code != 200) {
                     $res['error'] = true;
                     $res['errorMessage'] = isset($response->body->error_description) ? $response->body->error_description : $response->body->error;
                 }
             } else {
                 $res['error'] = true;
-                $res['errorMessage'] = $res['error_description'];
+                $res['errorMessage'] = $resp['error_description'];
             }
         } catch (ClientException $ce) {
             $res['error'] = true;
