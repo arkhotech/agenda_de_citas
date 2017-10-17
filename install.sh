08782ccb471ea14168d9ebe084db710c73430abf
Marcelo Silva <msilva@arkhotech.com>
2017-10-17 23:57:48 +0000

    CAmbioa Compose

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
-
 #!/bin/bash
 
 
 if [ -z $CONFIG_PATH ]; then
-	CONFIG_PATH="./application/local"
+	CONFIG_PATH="./"
 fi
 
 
@@ -14,7 +13,7 @@ function createConfig {
 	echo "APP_ENV=local" >> $CONFIG_PATH/.env
 	echo "APP_DEBUG=true" >> $CONFIG_PATH/.env
 	echo "APP_KEY=base64:vZa/paj/3EwfOPV7gL+pQypIOtJrkElCSZ9OSes28/o=" >> $CONFIG_PATH/.env
-	echo "APP_URL=http://localhost" >> $CONFIG_PATH/.env
+#	echo "APP_URL=http://localhost" >> $CONFIG_PATH/.env
 
 	echo "DB_CONNECTION=mysql" >> $CONFIG_PATH/.env
 	echo "DB_HOST=cal_database" >> $CONFIG_PATH/.env
@@ -23,7 +22,7 @@ function createConfig {
 	echo "DB_USERNAME=root" >> $CONFIG_PATH/.env
 	echo "DB_PASSWORD=qpalwosk10" >> $CONFIG_PATH/.env
 
-	echo "CACHE_DRIVER=file" >> $CONFIG_PATH/.env
+	echo "CACHE_DRIVER=redis" >> $CONFIG_PATH/.env
 	echo "SESSION_DRIVER=file" >> $CONFIG_PATH/.env
 	echo "QUEUE_DRIVER=sync" >> $CONFIG_PATH/.env
 
@@ -41,17 +40,3 @@ function createConfig {
 }
 
 createConfig
-
-docker-compose up -d 
-
-sleep 20
-
-echo "Inicializando base de datos"
-
-MYSQL_CONTAINER=`docker ps -f NAME=_cal_database -q`
-
-echo "Inicializando base de datos, instancia ${MYSQL_CONTAINER}"
-
-docker exec -i ${MYSQL_CONTAINER} /db-init.sh
-
-
